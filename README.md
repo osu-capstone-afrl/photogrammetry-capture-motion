b274a81c09e360c7dd7890e891426e919d2d46a9
acbuynak <acbuynak@gmail.com>
2021-01-21 02:13:26 -0500

    Initial Commit
    Includes first draft of readme and example python moveit commander script.

---
+++ README.md
@@ -0,0 +1,54 @@
+# Capture Tool for Photogrammetry Image Set :camera:
+Support package for capturing images from a DSLR camera mounted to the flange of a Motoman GP7. Intended to support a undergraduate capstone project on robotic inspection applications. Developed at The Ohio State University.
+
+_Note: This repository was designed for ROS Melodic. It has not been tested on other distributions._
+_Specifically designed for the Motoman GP7 robot as supported by the ROS-Industrial program._
+
+## Tools Contained
+* Inclined Plane Motion Method (for Motoman GP7)
+
+## Installation
+The following section explains how to setup the package to work.
+
+### Prerequisites
+  - **ROS Melodic:** For obtaining and configuring ROS follow the installation instructions for [full-desktop Melodic installation](http://wiki.ros.org/melodic/Installation/Ubuntu).
+  - **Catkin workspace:** Create a clean [catkin-tools](https://catkin-tools.readthedocs.io/en/latest/index.html) workspace.
+  - **MoveIt 1:** For installation instructions see [MoveIt's webpage](https://moveit.ros.org/install/).
+
+### Required Repositories
+  Clone the below repositories into your catkin-tools workspace:
+  - [photogrammetry-capture-motion](https://github.com/osu-capstone-afrl/photogrammetry-capture-motion)
+  - [ros-industrial/motoman](https://github.com/ros-industrial/motoman)
+
+
+### Dependencies
+To automatically install any missing dependencies of your ROS installation, run the following terminal commands:
+
+```
+#---------- install third party dependencies -----------
+sudo apt-get update
+# Move to the root of the workspace
+cd [PATH/TO/YOUR/WORKSPACE]
+# Install all dependencies of packages in the workspace
+rosdep install --from-paths src --ignore-src -r -y
+# Build your workspace
+catkin build
+source devel/setup.bash
+```
+Once the workspace build process is completed you are ready to start playing...cheers!
+
+#### Install missing dependencies
+If the build fails, it occurs usually to missing package dependencies or missing third party (non-ros) packages. When this occurs the build log in the terminal indicates the name of the package dependency that it is missing, then try:
+
+```
+sudo apt-get update ros-kinetic-[package-name]
+# separate the package name words with a '-'
+```
+If a package is not found it is probably a third-party dependency, google the name of the package and search for installation instructions:
+
+## Usage
+
+```
+roslaunch motoman_gp7_moveit_config moveit_planning_execution.launch sim:=true
+rosrun photogrammetry-capture-motion motion_inclined_plane.py
+```
---
+++ scripts/motion_inclined_plane.py
@@ -0,0 +1,322 @@
+#!/usr/bin/env python
+
+#####################################################
+##      Photogrammetry Photos Capture Tool         ##
+##                                                 ##
+##   Method: Inclined Planes                       ##
+##   * This is a ROS enabled node.                 ##
+##   * Initial Prototype                           ##
+##                                                 ##
+#####################################################
+
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2021, The Ohio State University
+# All rights reserved.
+#
+# Authors: Adam Buynak, Andrew Schellenberg
+
+#####################################################
+
+### IMPORTS
+#
+# `moveit_commander` namespace allows Python MoveIt interfaces.
+# Includes a `MoveGroupCommander`_, `PlanningSceneInterface`_, and `RobotCommander`_ class
+# 
+# Additional imports allow used for support, ROS messages, and etc.
+
+import sys
+import copy
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+from math import pi, radians
+from std_msgs.msg import String
+from moveit_commander.conversions import pose_to_list
+from motoman_msgs.srv import ReadSingleIO, WriteSingleIO
+
+## Quaternion Tools
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
+
+
+#####################################################
+## SUPPORT CLASSES AND FUNCTIONS
+##
+def all_close(goal, actual, tolerance):
+  """
+  Convenience method for testing if a list of values are within a tolerance of their counterparts in another list
+  @param: goal       A list of floats, a Pose or a PoseStamped
+  @param: actual     A list of floats, a Pose or a PoseStamped
+  @param: tolerance  A float
+  @returns: bool
+  """
+  all_equal = True
+  if type(goal) is list:
+    for index in range(len(goal)):
+      if abs(actual[index] - goal[index]) > tolerance:
+        return False
+
+  elif type(goal) is geometry_msgs.msg.PoseStamped:
+    return all_close(goal.pose, actual.pose, tolerance)
+
+  elif type(goal) is geometry_msgs.msg.Pose:
+    return all_close(pose_to_list(goal), pose_to_list(actual), tolerance)
+
+  return True
+
+
+class moveManipulator(object):
+  """moveManipulator Class"""
+  def __init__(self):
+    super(moveManipulator, self).__init__()
+
+    ## First initialize `moveit_commander`_ and a `rospy`_ node:
+    moveit_commander.roscpp_initialize(sys.argv)
+    rospy.init_node('node_moveManipulator', anonymous=True)
+
+    # Setup Variables needed for Moveit_Commander
+    self.glider_name = ''
+    self.robot = moveit_commander.RobotCommander()
+    self.scene = moveit_commander.PlanningSceneInterface()
+    self.group_name = "bot_gp7"
+    self.move_group = moveit_commander.MoveGroupCommander(self.group_name)
+    self.display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
+                                                   moveit_msgs.msg.DisplayTrajectory,
+                                                   queue_size=20)
+    self.planning_frame = self.move_group.get_planning_frame()
+    self.eef_link = self.move_group.get_end_effector_link()
+    self.group_names = self.robot.get_group_names()
+
+  def set_vel(self,max_vel):
+    ## Wrapper for Moveit Commander's max_velocity
+    ## Allowed range...   0.0 <= max_vel <= 1.0
+    self.move_group.set_max_velocity_scaling_factor(max_vel)
+  def set_accel(self,max_accel):
+    ## Wrapper for Moveit Commander's max_acceleration
+    ## Allowed range...   0.0 <= max_vel <= 1.0
+    self.move_group.set_max_acceleration_scaling_factor(max_accel)
+
+  def goto_all_zeros(self):
+    ## Go to "ALL-Zeros" position
+    ## Get Current Position & Go to "All-Zeros" Position
+    ## Trajectory Type: JOINT MOTION defined by joint position
+
+    # Get Current Position
+    joint_goal = self.move_group.get_current_joint_values()
+
+    # Define "All-Zeros" Position
+    joint_goal[0] = 0
+    joint_goal[1] = 0
+    joint_goal[2] = 0
+    joint_goal[3] = 0
+    joint_goal[4] = 0
+    joint_goal[5] = 0
+
+    # Send action to move-to defined position
+    self.move_group.go(joint_goal, wait=True)
+
+    # Calling ``stop()`` ensures that there is no residual movement
+    self.move_group.stop()
+
+    # For testing:
+    current_joints = self.move_group.get_current_joint_values()
+    return all_close(joint_goal, current_joints, 0.01)
+
+  def plan_cartesian_throw_path(self, scale=1):
+    ## Plan Cartesian Path to throw glider
+
+    # Specify a list of waypoints
+    waypoints = []
+
+    # Example - Commented Out
+    #wpose = self.move_group.get_current_pose().pose
+    #wpose.position.z += scale * 0.1  # Move up (z)
+    #wpose.position.x += scale * 0.8  # Forward (x)
+    #waypoints.append(copy.deepcopy(wpose))
+
+    # We want the Cartesian path to be interpolated at a resolution of 5 cm
+    # which is why we will specify 0.05 as the eef_step in Cartesian
+    # translation.  We will disable the jump threshold by setting it to 0.0,
+    # ignoring the check for infeasible jumps in joint space, which is sufficient
+    # for this tutorial.
+    (plan, fraction) = self.move_group.compute_cartesian_path(
+                                       waypoints,   # waypoints to follow
+                                       0.05,        # eef_step
+                                       0.0)         # jump_threshold
+
+    # Note: We are just planning, not asking move_group to actually move the robot yet:
+    return plan, fraction
+
+  def goto_Quant_Orient(self,pose):
+    ## GOTO Pose Using Cartesian + Quaternion Pose
+
+    # Get Current Orientation in Quanternion Format
+    # http://docs.ros.org/en/api/geometry_msgs/html/msg/Pose.html
+    #q_poseCurrent = self.move_group.get_current_pose().pose.orientation
+    #print(q_poseCurrent)
+
+    # Using Quaternion's for Angle
+    # Conversion from Euler(rotx,roty,rotz) to Quaternion(x,y,z,w)
+    # Euler Units: RADIANS
+    # http://docs.ros.org/en/melodic/api/tf/html/python/transformations.html
+    # http://wiki.ros.org/tf2/Tutorials/Quaternions
+    # http://docs.ros.org/en/api/geometry_msgs/html/msg/Quaternion.html
+
+    # Convert Euler Orientation Request to Quanternion
+    q_orientGoal = quaternion_from_euler(pose[3],pose[4],pose[5],axes='sxyz')
+
+    pose_goal = geometry_msgs.msg.Pose()
+    pose_goal.position.x = pose[0]
+    pose_goal.position.y = pose[1]
+    pose_goal.position.z = pose[2]
+    pose_goal.orientation.x = q_orientGoal[0]
+    pose_goal.orientation.y = q_orientGoal[1]
+    pose_goal.orientation.z = q_orientGoal[2]
+    pose_goal.orientation.w = q_orientGoal[3]
+
+    self.move_group.set_pose_target(pose_goal)
+
+    ## Call the planner to compute the plan and execute it.
+    plan = self.move_group.go(wait=True)
+
+    # Calling `stop()` ensures that there is no residual movement
+    self.move_group.stop()
+
+    # It is always good to clear your targets after planning with poses.
+    # Note: there is no equivalent function for clear_joint_value_targets()
+    self.move_group.clear_pose_targets()
+
+    # For testing:
+    current_pose = self.move_group.get_current_pose().pose
+    return all_close(pose_goal, current_pose, 0.01)
+
+  def goto_joint_posn(self,joint_goal):
+    ## Go to Joint Defined position
+    ## Get Current Position & Go to "All-Zeros" Position
+    ## Trajectory Type: JOINT MOTION defined by joint position
+
+    # Send action to move-to defined position
+    self.move_group.go(joint_goal, wait=True)
+
+    # Calling ``stop()`` ensures that there is no residual movement
+    self.move_group.stop()
+
+    # For testing:
+    current_joints = self.move_group.get_current_joint_values()
+    return all_close(joint_goal, current_joints, 0.01)
+
+  def act_gripper(self, request):
+    ## Wrapper for rosservice to open/close gripper using Read/Write IO
+
+    # Wait for ros services to come up
+    rospy.wait_for_service('read_single_io')
+    rospy.wait_for_service('write_single_io')
+
+    # Create Handle for Service Proxy's
+    try:
+        read_single_io = rospy.ServiceProxy('read_single_io', ReadSingleIO)
+        write_single_io = rospy.ServiceProxy('write_single_io', WriteSingleIO)
+    except rospy.ServiceException as e:
+        print("Gripper IO Service Call failed: %s"%e)
+
+    # Send 'Write' IO Message
+    try:
+      write_status = write_single_io(10010, request)
+    except:
+      print("An exception occured. Unable to write to Single IO.")
+
+
+    # Call Read Service to check current position
+    read_status = read_single_io(10011).value
+    if read_status:
+        print('Gripper is Closed')
+    else:
+        print('Gripper is Open')
+
+    return read_status
+
+  def execute_plan(self, plan):
+    ## Execute a Plan
+    ## Use execute if you would like the robot to follow a plan that has already been computed:
+    self.move_group.execute(plan, wait=True)
+
+  def wait_for_state_update(self, glider_is_known=False, glider_is_attached=False, timeout=4):
+    ## wait_for_scene_update
+    ## Serves to ensure that the paper-airplane simulated object has been attached to simulated robot model
+    ## This helps with collision planning. Not as important for paper-glider project, but is best practice.
+
+    start = rospy.get_time()
+    seconds = rospy.get_time()
+    while (seconds - start < timeout) and not rospy.is_shutdown():
+      # Test if the glider is in attached objects
+      attached_objects = self.scene.get_attached_objects([self.glider_name])
+      is_attached = len(attached_objects.keys()) > 0
+
+      # Test if the glider is in the scene.
+      # Note that attaching the glider will remove it from known_objects
+      is_known = self.glider_name in self.scene.get_known_object_names()
+
+      # Test if we are in the expected state
+      if (glider_is_attached == is_attached) and (glider_is_known == is_known):
+        return True
+
+      # Sleep so that we give other threads time on the processor
+      rospy.sleep(0.1)
+      seconds = rospy.get_time()
+
+    # If we exited the while loop without returning then we timed out
+    return False
+
+
+
+
+def main():
+  try:
+    print ""
+    print "----------------------------------------------------------"
+    print "            Photogrammetry Image Capture Tool             "
+    print "----------------------------------------------------------"
+    print "Program Developed by ACBUYNAK, SCHELLENBERG3. Spring 2021"
+    print "Press Enter to advance script when prompted."
+    print ""
+
+    # Initial Values & Controls
+    robot = moveManipulator()
+    robot.set_accel(0.2)
+    robot.set_vel(0.2)
+
+    # Move to Known Start Position: All-Zeros
+    raw_input('Go to All-Zeros Position <enter>')
+    robot.goto_all_zeros()
+
+    ## Example Cartesian Pose Instruction
+    raw_input('Go to Example Cart Pose <enter>')
+    pose_cart = [0.3,-0.4,0.8,0,radians(90),0]
+    robot.goto_Quant_Orient(pose_cart)
+
+    ## Example Joint Pose Instruction
+    raw_input('Go to Example Joint Pose <enter>')
+    pose_joint = [0.2,0.2,0.2,0.2,0.2,0.2]
+    robot.goto_joint_posn(pose_joint)
+
+    #raw_input('Begin Sequence <enter>')
+
+    ## Path Planning & Execution
+    # ############################
+    #
+    #
+    #
+    # ############################
+
+    raw_input('Per Best Practices, return to All-Zeros Joint Position <enter>')
+    robot.goto_all_zeros()
+
+    print "============ Complete!"
+  except rospy.ROSInterruptException:
+    return
+  except KeyboardInterrupt:
+    return
+
+if __name__ == '__main__':
+  main()
