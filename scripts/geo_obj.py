39dac9235856d302f35ec242bf224779a7158a25
schellenberg3 <schellenberg.andrew@gmail.com>
2021-01-26 12:07:29 -0500

    improve readability of function descriptions

--- scripts/geo_obj.py
+++ scripts/geo_obj.py
@@ -2,8 +2,9 @@ import numpy as np
 
 
 class Plane(object):
-    """Container for defining a plane by a list of x-by-z equally spaced points """
+    """ Container for defining a plane by a list of x-by-z equally spaced points """
     def __init__(self, x_num, z_num, x_len, z_len):
+        """ Initialization function """
         # internal
         self._x_num = x_num
         self._z_num = z_num
@@ -15,7 +16,7 @@ class Plane(object):
         self.points = self._get_points()
 
     def _get_points(self):
-        """ Returns all the points"""
+        """ Returns all the points on the plane. Attempts to do this in a path-planning friendly way """
         ret_list = []  # [[X0, Y0, Z0], [X1, Y1, Z1], ... ]  <- list of lists
         height = 0
         temp_list = [[0, 0, 0]]  # the temp list starts with the origin
@@ -35,6 +36,7 @@ class Plane(object):
 
     def rotate(self, axis, angle, rad=False):
         """ Rotates points around a specific axis by the specified angle """
+        # todo: decide if it is woth having a rotation around the plan's normal axis
         if axis not in ['x', 'X', 'y', 'Y', 'z', 'Z']:
             print "[WARN] Invalid axis in Plane.rotate(), original list was not changed"
             return
@@ -66,7 +68,6 @@ class Plane(object):
 
     def translate(self, axis, dist):
         """ Translates points around a specified axis by the specified distance """
-
         if axis not in ['x', 'X', 'y', 'Y', 'z', 'Z', 'n', 'N']:
             print "[WARN] Invalid axis in Plane.translate(), original list was not changed"
             return
@@ -89,7 +90,7 @@ class Plane(object):
         return
 
     def get_surf_norm(self):
-        """Returns the normal vector to the plane, scaled to unit length"""
+        """ Returns the normal vector to the plane, scaled to unit length """
         # todo: check if the directions are consistent with what we expect
         a = np.subtract(self.points[0], self.points[1])
         b = np.subtract(self.points[0], self.points[-1])
