b2d8b9f4337f4ec97669d5a1c0694dfc90b15348
acbuynak <acbuynak@gmail.com>
2021-02-18 02:01:12 -0500

    Add Visualization and Homogenous Transformation Support Tool Classes
    Tools pulled from AIMS Lab external code base with appropriate licenses included.

---
+++ scripts/visualizations.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+#####################################################
+##   Visualizations Support Class                  ##
+##                                                 ##
+##   Construct Visuals                             ##
+##   * Show path in 3D space                       ##
+##                                                 ##
+#####################################################
+
+# Software License Agreement (Apache 2.0 License)
+#
+# Copyright (c) 2021, The Ohio State University
+# Center for Design and Manufacturing Excellence (CDME)
+# The Artificially Intelligent Manufacturing Systems Lab (AIMS)
+# All rights reserved.
+#
+# Author: Adam Buynak
+
+#####################################################
+
+
+def plot_path_transforms(d2p):
+  import numpy as np
+  import matplotlib.pyplot as plt
+  from mpl_toolkits.mplot3d import Axes3D
+
+  fig = plt.figure()
+  ax = fig.add_subplot(111, projection='3d')
+
+  # Origin
+  ax.scatter(0, 0, 0)  # origin
+
+  # Collect Array of Just Vectors
+  vectors = np.zeros((len(d2p),3))
+  for i, transform in enumerate(d2p):
+    vectors[i,:] = transform[:-1,3]
+  #print(vectors)
+
+  # Path Plotted
+  for p in d2p:
+    ax.scatter(p[0,3], p[1,3], p[2,3], marker="x")
+  ax.plot(vectors[:, 0], vectors[:, 1], vectors[:, 2], color='b')
+
+  # Assign Labels
+  ax.set_xlabel('X-Axis')
+  ax.set_ylabel('Y-Axis')
+  ax.set_zlabel('Z-Axis')
+
+  # Display Plot
+  plt.show()
+
+  # Close Figure
+  #plt.close()
