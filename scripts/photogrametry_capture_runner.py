70b9de4c1a9cc04062f34ddd2ec3004753abe913
Andrew <schellenberg.andrew@gmail.com>
2021-04-13 17:11:17 -0400

    get detected object information from json file

--- scripts/photogrametry_capture_runner.py
+++ scripts/photogrametry_capture_runner.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
 from robot_support import moveManipulator
-from transformations import Transformations
 from path_plans import InclinedPlane
 from path_plans import SteppedRings
 import geometry_msgs.msg
 import rospy
 import time
+import json
 import sys
 import os
 # hacky code to import the camera module
@@ -26,7 +26,6 @@ if __name__ == '__main__':
         print "Press Enter to advance script when prompted."
         print ""
 
-
         ## SETUP ##
         # Initial Values & Controls
         robot = moveManipulator(input_planning_group = "eef_camera")
@@ -34,27 +33,24 @@ if __name__ == '__main__':
         robot.set_vel(0.1)
 
         # Move to Known Start Position: All-Zeros
-        #raw_input('Go to All-Zeros Position <enter>')
+        raw_input('Go to All-Zeros Position <enter>')
         robot.goto_all_zeros()
 
         ## PLANNING ##
         # Example detected object definition
-        tf = Transformations()
 
-        # size = [0.06, 0.06, 0.06]
-        # locator = [0.46, 0.0, 0.4]
-        object_size = [0.06, 0.14, 0.14]
-        object_posn = [0.48, 0.0, 0.32]
+        fname = os.path.join(current, "detected_object.json")
+        with open(fname, "r") as read_file:
+            detected_object = json.load(read_file)
 
-        orientation = tf.create_rotation_matrix([0],'z')
+        object_size = detected_object['size']
+        object_posn = detected_object['posn']
+        orientation = detected_object['orientation']
 
         # Add Object to Collision Planning Space
         robot.add_box_object(object_posn, object_size)
 
-        import numpy as np
-
         # create path plan
-        # plan = InclinedPlane(size, locator, orientation)
         # plan = InclinedPlane(object_size, object_posn, np.identity(3), count=(3,3), slope=0.2, clearance=0.06, offset=0.02)
         plan = SteppedRings(object_size, object_posn, orientation, scale=1.01, offset=0.01, level_count=2, density=7)
 
